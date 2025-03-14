24b4059ce38658906323a37920781cf893ea0de3
CamMV <j.murciav@uniandes.edu.co>
2025-03-14 01:53:29 -0500

    uri

--- eventos/urls.py
+++ eventos/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('', views.eventoList),
+    path('', views.eventoList, name = 'eventoList'),
     path('eventoCreate/', csrf_exempt(views.evento_create), name = 'eventoCreate')    
 ]
