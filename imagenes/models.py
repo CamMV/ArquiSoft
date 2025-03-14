1e7c6e8f36d1e4d0256cdc383a8efc789d2e370c
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-03-13 20:49:43 -0500

    a

--- imagenes/models.py
+++ imagenes/models.py
@@ -9,7 +9,6 @@ class EEG(models.Model):
 
 class MRI(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to="mri/")
     fecha = models.DateTimeField(auto_now_add=True)
 
 
@@ -21,7 +20,7 @@ class mRNA(models.Model):
 class Imagenes(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.CharField(max_length=250)
-    imagen = models.ImageField(upload_to='images/')
+
  
     def __str__(self):
         return f"{self.nombre}, {self.descripcion}"
