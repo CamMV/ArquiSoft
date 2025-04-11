842bba906b7b8d9d4d0d60df56a9bea70e3230e1
CamMV <j.murciav@uniandes.edu.co>
2025-04-10 20:13:24 -0500

    "Eliminación templetas no usables"

--- imagenes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-
-from .models import EEG, MRI, mRNA
-
-admin.site.register(EEG)
-admin.site.register(MRI)
-admin.site.register(mRNA)
--- imagenes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlptterns = [
-    path('', views.imagenesView, name = 'imagenes view'),
-    path('<int:pk>/', views.imagenView, name = 'imagen view'),
-]
