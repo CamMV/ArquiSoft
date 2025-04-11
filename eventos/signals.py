b57f68b22f58e8bae6e809c2341d0a3939f6776b
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-04-10 23:37:10 -0500

    Diferencia entre recomendaciones

--- eventos/signals.py
+++ eventos/signals.py
@@ -2,12 +2,18 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from eventos.models import Evento
 from resultados.models import Resultado
+import random
 
 @receiver(post_save, sender=Evento)
 def crear_resultado_automaticamente(sender, instance, created, **kwargs):
     if created:
+        valor = random.randint(1, 100)  # Genera un valor aleatorio entre 1 y 100
+        if valor< 50:
+            recomendaciones = "No se requiere tratamiento adicional."
+        else:
+            recomendaciones = "Se recomienda realizar un seguimiento."
         Resultado.objects.create(
             evento=instance,
-            valor=0,
-            recomendaciones="Pendiente de evaluación"
+            valor=valor,
+            recomendaciones=recomendaciones
         )
