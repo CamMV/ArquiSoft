38f61230966ffa77bf6d7540d9af49cff5525368
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-05-01 14:23:30 -0500

    gmail

--- diagnosticos/views.py
+++ diagnosticos/views.py
@@ -1,29 +1,28 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from .forms import DiagnosticoForm
 from django.contrib import messages
-from .logic.diagnosticosLogic import getDiagnosticos
 
-# Vista para listar todos los diagnósticos
-def diagnosticoList(request):
-    diagnosticos = getDiagnosticos()
-    context = {
-        'diagnosticos_list': diagnosticos
-    }
-    return render(request, 'Diagnostico/diagnosticos.html', context)
-
-# Vista para crear un diagnóstico
 def diagnostico_create(request):
     if request.method == 'POST':
         form = DiagnosticoForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Diagnóstico creado con éxito')
-            return redirect('diagnosticoCreate')  # Redirige para crear otro o podrías ir al listado
+            diagnostico = form.save()
+
+            send_mail(
+                subject='Nuevo diagnóstico creado',
+                message=f'Se ha creado un nuevo diagnóstico: {diagnostico.diagnostico}.',
+                from_email=None,  
+                recipient_list=['santigs2005@gmail.com', 'johan5murciav@gmail.com'],  # Los que recibirán el correo
+                fail_silently=False,
+            )
+
+            messages.success(request, 'Disponibilidad del diagnostico 99.9%')
+            return redirect('diagnosticoList')
     else:
         form = DiagnosticoForm()
     
     context = {
         'form': form
     }
-    return render(request, 'Diagnostico/diagnosticoCreate.html', context)
-
+    return render(request, 'diagnosticos/diagnosticoCreate.html', context)
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -135,3 +135,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration (producción)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'johan5murciav@gmail.com'  
+EMAIL_HOST_PASSWORD = 'irjh plia dzbj yqao' 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
