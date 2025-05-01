c309333bad5e8be88b0620df66e302a143204334
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-05-01 01:12:20 -0500

    diagnosticos

--- resultados/models.py
+++ resultados/models.py
@@ -1,9 +1,9 @@
 # resultados/models.py
 from django.db import models
-from eventos.models import Evento
+from dignosticos.models import Diagnostico
 
 class Resultado(models.Model):
-    evento = models.OneToOneField(Evento, on_delete=models.CASCADE, related_name='resultado')
+    diagnostico = models.OneToOneField(Diagnostico, on_delete=models.CASCADE, related_name='resultado')
     valor = models.IntegerField(default=0)
     recomendaciones = models.CharField(max_length=250)
     fecha_generacion = models.DateTimeField(auto_now_add=True)
