4f1a60b8f058d2d12cda8fd30aa2975cedb0be8b
Santiago Gomez <S.gomezo2@uniandes.edu.co>
2025-05-01 14:31:25 -0500

    create con send email

--- diagnosticos/views.py
+++ diagnosticos/views.py
@@ -1,8 +1,17 @@
-from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from .forms import DiagnosticoForm
 from django.contrib import messages
+from .logic.diagnosticosLogic import getDiagnosticos
 
+# Vista para listar todos los diagnósticos
+def diagnosticoList(request):
+    diagnosticos = getDiagnosticos()
+    context = {
+        'diagnosticos_list': diagnosticos
+    }
+    return render(request, 'Diagnostico/diagnosticos.html', context)
+
+# Vista para crear un diagnóstico
 def diagnostico_create(request):
     if request.method == 'POST':
         form = DiagnosticoForm(request.POST)
@@ -11,7 +20,7 @@ def diagnostico_create(request):
 
             send_mail(
                 subject='Nuevo diagnóstico creado',
-                message=f'Se ha creado un nuevo diagnóstico: {diagnostico.diagnostico}.',
+                message=f'Se ha creado un nuevo diagnóstico: {diagnostico.diagnostico}. con una disponibilidad del 99.9%',
                 from_email=None,  
                 recipient_list=['santigs2005@gmail.com', 'johan5murciav@gmail.com'],  # Los que recibirán el correo
                 fail_silently=False,
