11c1923bddf78b5c6f2dc219e17acd28410fa165
CamMV <j.murciav@uniandes.edu.co>
2025-05-02 00:14:23 -0500

    Cmabios get

--- diagnosticos/urls.py
+++ diagnosticos/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.diagnosticoList, name='diagnosticoList'),
     path('diagnosticocreate/', views.diagnostico_create, name='diagnosticoCreate'),
+    path('<int:diagnostico_id>/', views.diagnosticoInfo, name='diagnosticoInfo'),
+    path('email/', views.diagnosticoEmail, name='diagnosticoEmail'),
 ]
--- diagnosticos/views.py
+++ diagnosticos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import DiagnosticoForm
 from django.contrib import messages
-from .logic.diagnosticosLogic import getDiagnosticos
+from .logic.diagnosticosLogic import getDiagnosticos, getDiagnostico, createDiagnostico
+from django.http import JsonResponse
 from django.core.mail import send_mail
 
 # Vista para listar todos los diagnósticos
@@ -17,16 +18,7 @@ def diagnostico_create(request):
     if request.method == 'POST':
         form = DiagnosticoForm(request.POST)
         if form.is_valid():
-            diagnostico = form.save()
-
-            send_mail(
-                subject='Nuevo diagnóstico creado',
-                message=f'Se ha creado un nuevo diagnóstico: {diagnostico.diagnostico}. con una disponibilidad del 99.9%',
-                from_email=None,  
-                recipient_list=['santigs2005@gmail.com', 'johan5murciav@gmail.com'],  # Los que recibirán el correo
-                fail_silently=False,
-            )
-
+            createDiagnostico(form)
             messages.success(request, 'Disponibilidad del diagnostico 99.9%')
             return redirect('diagnosticoList')
     else:
@@ -36,3 +28,22 @@ def diagnostico_create(request):
         'form': form
     }
     return render(request, 'Diagnostico/diagnosticoCreate.html', context)
+
+def diagnosticoInfo(request, diagnostico_id):
+    if request.method == 'GET':
+        diagnostico = getDiagnostico(diagnostico_id)
+        return JsonResponse({"mensaje": "Diagnostico generado correctamente",
+                             "Diagnostico": diagnostico})
+    return JsonResponse({"error": "Metodo no permitido"}, status=405)
+
+def diagnosticoEmail(request):
+    if request.method == 'GET':
+        send_mail(
+                subject='Nuevo diagnóstico buscado',
+                message=f'Diagnostico buscado con una disponibilidad del 99.9%',
+                from_email=None,  
+                recipient_list=['santigs2005@gmail.com', 'johan5murciav@gmail.com'],  # Los que recibirán el correo
+                fail_silently=False,
+            )
+        return redirect('diagnosticoList')
+    return JsonResponse({"error": "Metodo no permitido"}, status=405)
